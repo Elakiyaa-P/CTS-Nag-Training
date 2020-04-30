9bbaa6b6b888ca0bd5bb899417a0cb65d4759766
Elakiyaa-P <elakiyaa5118@gmail.com>
2020-04-30 18:07:29 +0530

    arrow commit

--- 1-js-level-1/arrow_func.js
+++ 1-js-level-1/arrow_func.js
@@ -1,12 +1,12 @@
 console.log("Arrow Function")
 "use strict"
-// let getprice = () => 100 + 200
+let getprice = () => 100 + 200
 
-// let normalFunc = function() {
-//     console.log(this)
-// }
-// normalFunc();
-
+let normalFunc = function() {
+    console.log(this)
+}
+normalFunc();
+//Sample Arrow function
 let arrowFunc = () =>
     console.log(this)
 arrowFunc();
